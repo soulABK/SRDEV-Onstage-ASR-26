0737cacf850cf80040b1d6d2514aa40c0ecbb6a1
George Deng <dcx11dcy17@gmail.com>
2026-09-20 17:48:10 -0400

    Refactor IMU rotation update and calibration methods
    Updated the IMU rotation handling and calibration process. Improved rotation calculation and added a check for sensor initialization in calibration.

--- imu.cpp
+++ imu.cpp
@@ -1,6 +1,6 @@
 #include "imu.h"
 
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire1);
 
 int rotation = 0;
 int zeroRot = 0;
@@ -9,17 +9,15 @@ void updateRotation() {
   sensors_event_t event;
   bno.getEvent(&event);
   int raw = ((int) event.orientation.x);
-  rotation = raw - zeroRot;
-  if (rotation > 360) {
-    rotation = rotation - 360;
-  }
-  if (rotation < 0) {
-    rotation = rotation + 360;
-  }
-  rotation = zeroRot + ((rotation > 180) ? (rotation - 360) : rotation); 
+  rotation = raw + zeroRot;
+  rotation = (rotation + 360) % 360;
+  rotation = (rotation > 180) ? (rotation - 360) : rotation; 
 }
 
-void calibrateRotation() {
+bool calibrateRotation() {
+  if (!bno.begin()) {
+    return false;
+  }
   int totalRot = 0;
   for (int i = 0; i < IMU_CALIB_SAMPLES; i++) {
     updateRotation();
@@ -27,4 +25,5 @@ void calibrateRotation() {
     delay(IMU_CALIB_DELAY);
   }
   zeroRot = (int)round(-(float)totalRot / IMU_CALIB_SAMPLES);
+  return true;
 }
